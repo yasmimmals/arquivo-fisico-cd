0e1cc944f32f539fdbe60953030184819dc75cf7
Yasmim Alves Ferreira <yasmimalsferreira@gmail.com>
2026-06-23 11:42:34 -0300

    Atualiza import do Prisma no script de fix

--- prisma/fix-nome-documento.ts
+++ prisma/fix-nome-documento.ts
@@ -1,6 +1,6 @@
 import 'dotenv/config'
 import { PrismaPg } from '@prisma/adapter-pg'
-import { PrismaClient } from '../src/generated/prisma/client'
+import { PrismaClient } from '@prisma/client'
 import pg from 'pg'
 import fs from 'fs'
 import path from 'path'
